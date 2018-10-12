460e167bda51f3308f15274a48ac31014b8b01a4
Natasha S Love <debora@inbox.ru>
2018-10-12 09:56:16 -0500

    new method in new class

---
+++ ParadiseInfo2.java
@@ -0,0 +1,15 @@
+public class ParadiseInfo {
+	
+	public static void main (String [] args) {
+		
+		displayInfo();
+		
+	}
+	
+	public static void displayInfo(){
+		System.out.println();
+		System.out.println("Paradise Day SPA wants to pamper you.");
+		System.out.println("We'll make you look good.");
+	}
+	
+}
