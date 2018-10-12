c1fb54d5b44900cea3fd4d89f9d3514f175750c9
Natasha S Love <debora@inbox.ru>
2018-10-12 09:26:26 -0500

    Initial commit

---
+++ ParadiseInfo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ParadiseInfo {
+	
+	public static void main (String [] args) {
+		
+		displayInfo();
+		
+	}
+	
+	public static void displayInfo(){
+		
+		System.out.println("Paradise Day SPA wants to pamper you.")
+				System.out.println("We'll make you look good.")
+	}
+	
+}
